56bfc449c3566e840f5a43f012254d8386395d1f
developeranjuUI <116105555+developeranjuUI@users.noreply.github.com>
2025-03-29 17:27:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let count = 0;
+let counter = document.getElementById('counter');
+let incrementBtn = document.getElementById('incrementBtn');
+
+incrementBtn.addEventListener('click',()=>{
+	count++;
+	counter.textContent = count;
+	alert(count);
+})
